41a80c5205d6add0bd53eff295905ec269ae4516
shubham <jethurishubham@gmail.com>
2020-11-03 18:56:58 +0530

    Initial commit

---
+++ app/src/main/java/com/smartherd/covid19tracker/EachDistrictDataActivity.java
@@ -0,0 +1,14 @@
+package com.smartherd.covid19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EachDistrictDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_each_district_data);
+    }
+}
